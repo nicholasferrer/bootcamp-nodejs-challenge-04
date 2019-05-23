703f08d2b79eb5f9f8a88fade1b9d42cd844de3d
Nicholas Ferrer <nicholasferrer@hotmail.com>
2019-05-23 15:24:15 -0300

    first commit

---
+++ app/Models/User.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class User extends Model {
+  static boot() {
+    super.boot();
+
+    this.addHook("beforeSave", async userInstance => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password);
+      }
+    });
+  }
+
+  tokens() {
+    return this.hasMany("App/Models/Token");
+  }
+}
+
+module.exports = User;
